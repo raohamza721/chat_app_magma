05939bb6f9e97690c4a543dfc445581210b02164
raohamza721 <141331947+raohamza721@users.noreply.github.com>
2024-10-23 11:27:42 +0500

    splash Screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:talk_shalk/screens/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    Future.delayed(Duration(seconds: 3), () {
+      Get.offAll(LoginScreen());  // Navigates to HomeScreen and clears the stack
+    });
+
+
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 90,width: 90,
+            child: Center(child: Image.asset('assets/images/icons/splashlogo.png'))),
+      ),
+
+    );
+  }
+}
